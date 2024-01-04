eb55902ab25b28b29f47f53a3ff46c6e6d0a9bd2
47173 <47173@DESKTOP-2F88QVD>
2024-01-04 17:32:27 +0800

    2024.1.4

---
+++ JavaWeb/test.js
@@ -0,0 +1,48 @@
+function initSetup(vm) {
+    var options = vm.$options;
+    var setup = options.setup;
+    if (setup) {
+        var ctx = (vm._setupContext = createSetupContext(vm));
+        setCurrentInstance(vm);
+        pushTarget();
+        var setupResult = invokeWithErrorHandling(setup, null, [vm._props || shallowReactive({}), ctx], vm, "setup");
+        popTarget();
+        setCurrentInstance();
+        if (isFunction(setupResult)) {
+            // render function
+            // @ts-ignore
+            options.render = setupResult;
+        }
+        else if (isObject(setupResult)) {
+            // bindings
+            if (setupResult instanceof VNode) {
+                warn$2("setup() should not return VNodes directly - " +
+                    "return a render function instead.");
+            }
+            vm._setupState = setupResult;
+            // __sfc indicates compiled bindings from <script setup>
+            if (!setupResult.__sfc) {
+                for (var key in setupResult) {
+                    if (!isReserved(key)) {
+                        proxyWithRefUnwrap(vm, setupResult, key);
+                    }
+                    else {
+                        warn$2("Avoid using variables that start with _ or $ in setup().");
+                    }
+                }
+            }
+            else {
+                // exposed for compiled render fn
+                var proxy = (vm._setupProxy = {});
+                for (var key in setupResult) {
+                    if (key !== '__sfc') {
+                        proxyWithRefUnwrap(proxy, setupResult, key);
+                    }
+                }
+            }
+        }
+        else if (setupResult !== undefined) {
+            warn$2("setup() should return an object. Received: ".concat(setupResult === null ? 'null' : typeof setupResult));
+        }
+    }
+}
